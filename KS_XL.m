87593b98b96afaa16cc6665732f68fb8bb568992
Xueying Liu <xueying.liu@uni-bonn.de>
2016-12-11 21:30:20 +0100

    add 3 matlab code, for ichimura estimator, klein spady estimator and simulation respectively

---
+++ KS_XL.m
@@ -0,0 +1,35 @@
+function beta = KS_XL(X,Y,h,beta0)
+
+[n,k]= size(X);
+
+%%
+    function value = Objfun(X,Y,beta,h)
+        
+        Xb = X*beta;
+
+        Kernel = @(u) 3/4 * (1-u.^2).^2.*(abs(u)<=1); %use Epanechnikov Kernel
+        G = zeros(n,1);
+
+        for i=1:n
+            dis = Xb - Xb(i);
+            K = Kernel(dis/h);
+            G(i) = K'* Y/sum(K);
+        end
+        
+        value = sum(Y.*log(G)+(ones(n,1)-Y).* log(ones(n,1)-G));
+        
+    end
+
+%set minimization options, set constrait tolerance to 1 for normalization of first coeff.
+options=optimset('MaxFunEvals', 1000, 'MaxIter', 5000,'TolX',1e-10,'TolFun',1e-10, 'TolCon',1e-10);
+con = [1,zeros(1,k-1)]; % Normalize the first coeff. to 1
+
+%minimize function 'Objfun' w.r.t. beta, with starting value beta0,
+%linear constraint set to con*beta=1 such that first coeff. is normalized to 1
+%lower bound and upper bound of beta set to -inf and inf respectively
+beta = fmincon(@(beta)Objfun(X,Y,beta,h), beta0, [], [],con, 1, -inf, inf,[],options); 
+
+
+end
+
+
