8ba5b4d11f76c1c64ecc71ebba5307a65eeb65fa
Xueying Liu <xueying.liu@uni-bonn.de>
2016-12-17 17:23:44 +0100

    edit ichimura and add klein spady estimators, with normalization, including monte carlo

--- KS.R
+++ KS.R
@@ -1,15 +1,16 @@
 # Set parameters for true model
 set.seed(109)
 N<-300
-x_1<-rnorm(N,mean=10,sd=1.5)
-x_2<-rnorm(N,mean=5,sd=1)
+x_1<-rnorm(N,mean=-7,sd=2.5)
+x_2<-rnorm(N,mean=1,sd=.5)
 X<-cbind(x_1,x_2)
 beta_vec<-c(1,7.6)
-m<-X %*% beta_vec
 
-# Construct reponsive variable y with function sine based on single index m 
-# and uniformly distributed errors.
-y<-sin(m)+runif(N,-1,1)
+# assume true distribution function g of the error is normal
+e<- rnorm(N,mean=0,sd=1)
+m<-X %*% beta_vec + e 
+y = m>0         
+
 
 #select bandwidth for estimator
 bandwidth<-.1
@@ -37,22 +38,22 @@ g_i <- function(X,y,b_vec,h) {
 
 # Define the objective minimizing function of sum-of-squared errors,
 # Use Gaussian kernel function.
-min_SSE_Gaussian <- function(X,y,b_vec,h) {
+loglike <- function(X,y,b_vec,h) {
   g_i <- (1:N)*NA
   for (i in 1:N){
     u <- sweep(X,2,X[i,])[-i,]%*%b_vec
     kde=normal_kde(u,h)
     estimate <- y[-i]%*%kde / sum(kde)
     g_i[i] <- estimate
-    sse <-t(y-g_i)%*%(y-g_i)    #only this line is different from the function g_i
+    L <-log(g_i+0.0000001)%*%y + log(1-g_i+0.0000001)%*%(1-y)    #only this line is different from the function g_i
     }
-  return(sse)
+  return(-L)
 }
 
 
 
 # Minimize the objective function, look for a best extimate for beta_vec.
-estimates=nlm(min_SSE_Gaussian,c(1,1),X=X,y=y,h=bandwidth)$estimate
+estimates=nlm(loglike,c(1,10),X=X,y=y,h=bandwidth)$estimate
 estimates_norm = estimates/estimates[1]
 
 #approximate function g
@@ -68,22 +69,24 @@ plot(m,g(m))
 
 
 #monte carlo
-M = 100   #number of monte carlo simulation
-n = 30    # Sample Size
-z <- rep(NA,M)
+M = 50   #number of monte carlo simulation
+n = 20    # Sample Size
+z <- rep(NA, M)
 beta_vec<-c(1,7.6) #true beta
 
 
 ## MC-Experiments:
 for(j in 1:M){
 ##Generate x and y sample:
-  x_1<-rnorm(N,mean=10,sd=1.5)
-  x_2<-rnorm(N,mean=5,sd=1)
+  x_1<-rnorm(N,mean=-7,sd=2.5)
+  x_2<-rnorm(N,mean=1,sd=.5)
   X<-cbind(x_1,x_2)
-  m<-X %*% beta_vec
-  y<-sin(m)+runif(N,-1,1)
+# assume true distribution function g of the error is normal
+  e<- rnorm(N,mean=0,sd=1)
+  m<-X %*% beta_vec + e 
+  y = m>0 
 ## Compute jth realization of Z:
-  estimates=nlm(min_SSE_Gaussian,c(1,1),X=X,y=y,h=bandwidth)$estimate
+  estimates=nlm(loglike,c(1,4),X=X,y=y,h=bandwidth)$estimate
   estimates_norm = estimates/estimates[1]
   z[j] <- estimates_norm[2] 
 }
