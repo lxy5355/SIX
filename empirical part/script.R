c584300984f738fcdfd57e2c1e6b136b7c98dfad
Xueying Liu <xueying.liu@uni-bonn.de>
2016-12-27 23:01:46 +0100

    add data voice ańd baseline r script for empirical part of the paper

---
+++ empirical part/script.R
@@ -0,0 +1,48 @@
+library(caTools)
+#library(rpart)
+#library(rpart.plot)
+
+data <- read.csv('c:/Users/lxy53/Desktop/voice.csv')
+
+# Create a train and test set.
+set.seed(777)
+spl <- sample.split(data$label, 0.7)
+train <- subset(data, spl == TRUE)
+test <- subset(data, spl == FALSE)
+
+###############
+# Basline model (always predict male).
+###############
+
+print('Baseline model')
+
+# Accuracy: 0.50
+table(train$label)
+1109/nrow(train)
+
+# Accuracy: 0.50
+table(test$label)
+475/nrow(test)
+
+###################################
+# Logistic regression model.
+###################################
+
+print('Logistic regression model')
+
+genderLog <- glm(label ~ ., data=train, family='binomial')
+
+# Accuracy: 0.9711
+predictLog <- predict(genderLog, type='response')
+table(train$label, predictLog >= 0.5)
+(1073+1081)/nrow(train)
+
+# Accuracy: 0.9789
+predictLog2 <- predict(genderLog, newdata=test, type='response')
+table(test$label, predictLog2 >= 0.5)
+(462+468)/nrow(test)
+
+##################################
+#ichimura methods
+##################################
+
