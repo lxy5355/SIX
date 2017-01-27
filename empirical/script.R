d7b1cc0a802b9ce275b19b4af6a9a2651e86c836
Xueying Liu <xueying.liu@uni-bonn.de>
2017-01-27 13:04:35 +0100

    finished first draft with empirical part, results looks okay, not amazing

--- empirical/script.R
+++ empirical/script.R
@@ -2,8 +2,9 @@ library(caTools)
 #library(rpart)
 #library(rpart.plot)
 source ("ichimura_functions.R")
+source ("KS_functions.R")
 
-data <- read.csv('C:/Users/lxy53/Documents/GitHub/SIX/empirical part/voice.csv')
+data <- read.csv('C:/Users/lxy53/Documents/GitHub/SIX/empirical/voice.csv')
 
 # Create a train and test set.
 set.seed(777)
@@ -11,6 +12,7 @@ spl <- sample.split(data$label, 0.7)
 train <- subset(data, spl == TRUE)
 test <- subset(data, spl == FALSE)
 
+h=.1
 ###############
 # Basline model (always predict male).
 ###############
@@ -46,4 +48,60 @@ table(test$label, predictLog2 >= 0.5)
 ##################################
 #ichimura methods
 ##################################
-genderLog <- ichimura
+
+ytrain = train$label == "male"  #convert y data to binary
+ytest = test$label == "male"
+
+xtrain = as.matrix( within(train,rm(label)) )
+colnames(xtrain) = NULL
+rownames(xtrain) = NULL
+xtest = as.matrix(  within(test,rm(label))  )
+colnames(xtest) = NULL
+rownames(xtest) = NULL
+
+ichimura <- ichimura_calc (xtrain,ytrain,h)
+g.hat.ichimura = ichimura$g.hat
+beta.hat.ichimura = ichimura$beta.hat
+
+# accuracy rate for training sample
+y.hat = g.hat.ichimura(xtrain%*%beta.hat.ichimura)>.5
+table(ytrain, y.hat)
+(1030+1045)/nrow(xtrain)
+
+# accuracy rate for test sample
+y.test.hat = g.hat.ichimura(xtest%*%beta.hat.ichimura)>.5
+table(ytest, y.test.hat)
+(446+451)/nrow(xtest)
+
+##################################
+#KS methods
+##################################
+KS <- KS_calc (xtrain,ytrain,h)
+g.hat.KS = KS$g.hat
+beta.hat.KS = KS$beta.hat
+
+# accuracy rate for training sample
+y.hat.KS = g.hat.KS(xtrain%*%beta.hat.KS)>.5
+table(ytrain, y.hat.KS)
+(1022+1030)/nrow(xtrain)
+
+# accuracy rate for test sample
+y.test.hat.KS = g.hat.KS(xtest%*%beta.hat.KS)>.5
+table(ytest, y.test.hat.KS)
+(440+441)/nrow(xtest)
+
+##########################################################################
+#try out code
+
+grid.min <- min(genderLog$coefficients,na.rm=TRUE)
+grid.max <- max(genderLog$coefficients,na.rm=TRUE)
+
+
+x1 = rnorm(100)
+x2 = rnorm(100)
+x3 = rnorm(100)
+x4 = rnorm(100)
+y = x1*2+x2*3+x3*4+x4*5>0
+x=cbind(x1,x2,x3,x4)
+ichimura = ichimura_calc(X=x,y=y,h=.2)
+
