a06d7a791ceb29333ffee59060a1051d22321198
xi sun <sunxi0227@gmail.com>
2017-02-26 16:11:45 +0100

    improve figure for comparison between np and self implementation function

--- monte_carlo_simulation/np_package_vs_functions/compare_graphs.R
+++ monte_carlo_simulation/np_package_vs_functions/compare_graphs.R
@@ -5,11 +5,16 @@ beta_ks_np=read.csv("mc_beta_ks_np.csv",header=TRUE)
 
 
 #########################################################################
+beta_hat_ich_self <- beta_ich_fun$x
+beta_hat_ich_np <- beta_ich_np$x
+beta_hat_ks_self <- beta_ks_fun$x
+beta_hat_ks_np <- beta_ks_np$x
+
 par(mfrow=c(2,2))
-hist(beta_ich_fun$x,breaks = 50, main="beta_ich_fun",xlim = range(c(-4,-1)))
-hist(beta_ks_fun$x,breaks = 50, main="beta_ks_fun",xlim = range(c(-4,-1)))
-hist(beta_ich_np$x,breaks =60, main="beta_ich_np", xlim = range(c(-4,-1)))
-hist(beta_ks_np$x,breaks = 60, main="beta_ks_np", xlim = range(c(-4,-1)))
+hist(beta_hat_ich_self,breaks = 50, main="Ichimura:self-implementation",xlim = range(c(-4,-1)))
+hist(beta_hat_ks_self,breaks = 50, main="Klein and Spady:self-implementation",xlim = range(c(-4,-1)))
+hist(beta_hat_ich_np,breaks =60, main="Ichimura:np-package", xlim = range(c(-4,-1)))
+hist(beta_hat_ks_np,breaks = 60, main="Klein and Spady:np-package", xlim = range(c(-4,-1)))
 
 boxplot(beta_ich_fun$x, main="beta_ich_fun")
 boxplot(beta_ich_np$x, main="beta_ich_np")
