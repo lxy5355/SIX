3823d8a7ca05908e5b989d4e3bda3d5dc3d1fb29
Xueying Liu <xueying.liu@uni-bonn.de>
2017-03-10 10:53:44 +0100

    improved plotting, added more models to show consistency, everything working now. need to add table

--- project_paper/src/final/plot_beta_hat.py
+++ project_paper/src/final/plot_beta_hat.py
@@ -4,6 +4,7 @@
 import sys
 import pickle
 import matplotlib.pyplot as plt
+import numpy as np
 
 from bld.project_paths import project_paths_join as ppj
 
@@ -13,15 +14,17 @@ if __name__ == "__main__":
 
     with open(ppj("OUT_ANALYSIS", "simulation_{}.pickle".format(model_name)), "rb") as in_file:
         beta_hat = pickle.load(in_file)
-                
+        
+    bins = np.linspace(-5, 0, 50, endpoint=True)
+
     plt.figure()
-    plt.hist(beta_hat['ichimura'][model_name],bins=10)
+    plt.hist(beta_hat['ichimura'][model_name],bins)
     plt.savefig(ppj("OUT_FIGURES", 'beta_hat_ichimura_{}.png'.format(model_name)))
 
     plt.figure()
-    plt.hist(beta_hat['KS'][model_name],bins=10)
+    plt.hist(beta_hat['KS'][model_name],bins)
     plt.savefig(ppj("OUT_FIGURES", 'beta_hat_KS_{}.png'.format(model_name)))
 
     plt.figure()
-    plt.hist(beta_hat['log'][model_name],bins=10)
+    plt.hist(beta_hat['log'][model_name],bins)
     plt.savefig(ppj("OUT_FIGURES", 'beta_hat_log_{}.png'.format(model_name)))
